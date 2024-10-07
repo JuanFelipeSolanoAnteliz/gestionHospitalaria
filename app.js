8145a09f09843e16ba343b15809b7230197e2656
JuanFelipeSolanoAnteliz <juanfesolant0906@gmail.com>
2024-10-07 08:58:21 -0500

    feat: :sparkles: index router

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const { join,dirname } = require('path');
+const indexRouter = require('./server/router/indexRouter');
+
+app.get('/', (req, res, next)=>{
+    req.__dirname = __dirname;
+    next();
+}, indexRouter);
+
+const config = {
+    port: process.env.EXPRESS_PORT,
+    host: process.env.EXPRESS_HOST_NAME
+};
+
+app.listen(config,()=>{
+    console.log(`server runnin at ${process.env.EXPRESS_PROTOCOL}${process.env.EXPRESS_HOST_NAME}:${process.env.EXPRESS_PORT}`);
+});
---
+++ server/router/indexRouter.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const layout = require('../view/indexRouter');
+
+router.use(layout);
+
+module.exports = router;
